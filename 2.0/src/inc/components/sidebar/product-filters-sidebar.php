95342a911b1e70c240b9af07d7258d6e674687f9
aslamahamed13 <aslamahamed13@gmail.com>
2018-05-29 16:18:52 +0530

    2.0

---
+++ 2.0/src/inc/components/sidebar/product-filters-sidebar.php
@@ -0,0 +1,39 @@
+<aside class="widget widget_electro_products_filter">
+    <h3 class="widget-title">Filters</h3>
+    <aside class="widget woocommerce widget_layered_nav">
+        <h3 class="widget-title">Brands</h3>
+        <ul>
+            <li style=""><a href="#">Apple</a> <span class="count">(4)</span></li>
+            <li style=""><a href="#">Gionee</a> <span class="count">(2)</span></li>
+            <li style=""><a href="#">HTC</a> <span class="count">(2)</span></li>
+            <li style=""><a href="#">LG</a> <span class="count">(2)</span></li>
+            <li style=""><a href="#">Micromax</a> <span class="count">(1)</span></li>
+        </ul>
+        <p class="maxlist-more"><a href="#">+ Show more</a></p>
+    </aside>
+    <aside class="widget woocommerce widget_layered_nav">
+        <h3 class="widget-title">Color</h3>
+        <ul>
+            <li style=""><a href="#">Black</a> <span class="count">(4)</span></li>
+            <li style=""><a href="#">Black Leather</a> <span class="count">(2)</span></li>
+            <li style=""><a href="#">Turquoise</a> <span class="count">(2)</span></li>
+            <li style=""><a href="#">White</a> <span class="count">(4)</span></li>
+            <li style=""><a href="#">Gold</a> <span class="count">(4)</span></li>
+        </ul>
+        <p class="maxlist-more"><a href="#">+ Show more</a></p>
+    </aside>
+    <aside class="widget woocommerce widget_price_filter">
+        <h3 class="widget-title">Price</h3>
+        <form action="#" method="get">
+            <div class="price_slider_wrapper">
+                <div id="slider-range" class="price_slider"></div>
+                <div class="price_slider_amount">
+                    <div class="price_label"> Price: <span class="from">$50</span> — <span class="to">$3,485</span>
+                        <input type="text" id="amount" readonly style="border:0;">
+                    </div>
+                    <button type="submit" class="button">Filter</button>
+                </div>
+            </div>
+        </form>
+    </aside>
+</aside>
